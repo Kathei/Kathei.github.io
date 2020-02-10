588cff1ed36cc903b8c533beae6eeea0d30cc70a
Katriina Heiskanen <katriina.heiskanen@aalto.fi>
2020-02-10 21:11:09 +1000

    Updates

---
+++ precache-manifest.b82baf04e316dc7fdeb011bf479e466f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3658a840c16ec02c4bf237294a0cc53d",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "0fd3120d12958c6e954f",
+    "url": "/portfolio-react/static/css/2.e2c7a280.chunk.css"
+  },
+  {
+    "revision": "8eb29b5d15007fdbf29a",
+    "url": "/portfolio-react/static/css/main.c0de3aae.chunk.css"
+  },
+  {
+    "revision": "0fd3120d12958c6e954f",
+    "url": "/portfolio-react/static/js/2.a5e0dd74.chunk.js"
+  },
+  {
+    "revision": "9b2fe38163b36d6a6bf4e13c4aa2d7fa",
+    "url": "/portfolio-react/static/js/2.a5e0dd74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb29b5d15007fdbf29a",
+    "url": "/portfolio-react/static/js/main.ee3bd340.chunk.js"
+  },
+  {
+    "revision": "2cfe62a8b4dce97521d1",
+    "url": "/portfolio-react/static/js/runtime-main.757d4273.js"
+  },
+  {
+    "revision": "dff97d8e379b0902e9106c87067dd94b",
+    "url": "/portfolio-react/static/media/Heiskanen_Katriina_2_1.dff97d8e.jpg"
+  },
+  {
+    "revision": "bd6ef033987018330e182124f34324ca",
+    "url": "/portfolio-react/static/media/IMG_0466.bd6ef033.jpg"
+  }
+]);
